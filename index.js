b6288ca688f6e2cbedb8675ab6c6fae593f87c89
Sean Martin <sean@seanmartinstudio.com>
2022-01-19 12:45:21 -0800

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+const { consoleLog } = require("mocha/lib/reporters/base")
 
+function isPalindrome(word) {
+  const wordArray = word.split('')
+  const wordArrayReverse = wordArray.reverse()
+  const wordArrayReverseString = wordArrayReverse.join('')
+  if(word === wordArrayReverseString) {
+    return true
+  } else {
+    return false
+  } 
+}
+isPalindrome('mom')
 /* 
   Add your pseudocode here
+  1. Declare isPalindrome function
+  2. isPalindrome needs to accept a string as an argument
+  3. Take passed in string and store in variable
+  4. Make a copy of the string and reverse the order of the letters
+  5. Wite a conditional to evaluate whether the string and backwords string match (is statement?)
 */
 
 /*
   Add written explanation of your solution here
+  
 */
 
-// You can run `node index.js` to view these console logs
+// // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
